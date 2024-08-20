885bb5c19d2515b319a316bbbe6e1262289f6d71
jo <158077622+joxehh@users.noreply.github.com>
2024-08-20 10:36:04 +0000

    Variable

---
+++ Basics_01/Variables.js
@@ -0,0 +1,7 @@
+/*there are three types of vairiables in javascript var, let, const.*/
+
+const accountId="23442123";
+let accountName="Jo";
+var email="joby@gmail.com";
+
+console.table([accountId, accountName, email]);
