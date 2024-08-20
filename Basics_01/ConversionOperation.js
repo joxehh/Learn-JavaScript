959cbe345a9dcd0ef8ad38c8ac96389b2a30ee7e
jo <158077622+joxehh@users.noreply.github.com>
2024-08-20 11:09:50 +0000

    coversions

---
+++ Basics_01/ConversionOperation.js
@@ -0,0 +1,10 @@
+// in javascript you can convert any data type into your desired datatype 
+let score="22"
+console.log(typeof score);
+console.log(score);
+
+let covert=Number(score);
+console.log(typeof covert);
+console.log(covert)
+
+// what if we try to convert alphabets in a string into a number
