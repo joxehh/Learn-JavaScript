1e757901c06bc9e139e5df809f13aa269359aad8
jo <158077622+joxehh@users.noreply.github.com>
2024-09-02 10:56:40 +0000

    more conversionso of datatypes

--- Basics_01/ConversionOperation.js
+++ Basics_01/ConversionOperation.js
@@ -6,5 +6,58 @@ console.log(score);
 let covert=Number(score);
 console.log(typeof covert);
 console.log(covert)
+console.log("*******")
 
 // what if we try to convert alphabets in a string into a number
+let alphabet="moshpit"
+console.log(typeof alphabet)
+let StringIntoNum=Number(alphabet)
+console.log(typeof StringIntoNum)
+console.log(StringIntoNum)
+console.log("*******")
+
+let Boolean=true
+console.log(typeof Boolean)
+let BooleanIntoNum=Number(Boolean)
+console.log(typeof BooleanIntoNum)
+console.log(BooleanIntoNum)
+console.log("*******")
+
+let Empty=null
+console.log(Empty)
+console.log(typeof Empty);
+let NullIntoNum=Number(Empty)
+console.log(NullIntoNum)
+console.log(typeof NullIntoNum);
+console.log("*******")
+
+let unknown=undefined
+console.log(unknown);
+console.log(typeof unknown);
+let UndefinedIntoNum=Number(unknown)
+console.log(UndefinedIntoNum);
+console.log(typeof UndefinedIntoNum);
+console.log("*******")
+
+//lets convert number into boolean
+
+let YouAreGay=1
+console.log(YouAreGay);
+console.log(typeof YouAreGay);
+let AreYouGay=Boolean(YouAreGay)
+console.log(AreYouGay);
+console.log(typeof AreYouGay)
+console.log("*******")
+
+
+
+
+
+
+
+
+
+
+
+
+
