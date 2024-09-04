a7aa5a9017263bbdc8e915dc21e2e0f820e4b50a
jo <158077622+joxehh@users.noreply.github.com>
2024-09-04 11:13:13 +0000

    comparisions in console.log()

---
+++ Basics_01/comparision.js
@@ -0,0 +1,10 @@
+console.log(2>1);
+console.log(2>=1);
+//by comparing value in console.log() we get boolean value
+
+console.log(null==0);
+console.log(null>=0);
+console.log(null<=0);
+console.log(null==0);
+
+
