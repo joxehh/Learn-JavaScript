551ec963b1de539f9dc884489f46adc44a7e0db8
jo <158077622+joxehh@users.noreply.github.com>
2024-09-04 11:39:34 +0000

    datatypes summary

---
+++ Basics_01/datatypes summary.js
@@ -0,0 +1,30 @@
+// there are two main catogarization of datatypes
+//#1 Primitive Datatypes
+
+// there are 7 types of primitive datatypes: String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+//String
+let name="Jo"
+console.log(name);
+console.log(typeof name);
+
+//Number
+let score=100
+console.log(score);
+console.log(typeof score);
+
+//Boolean
+let AreYouGay=true
+console.log('AreYouGay: ${AreYouGay}');
+console.log(typeof AreYouGay);
+
+/*Null
+let OutsideTemp=Null
+console.log(OutsideTemp);
+console.log(typeof OutsideTemp);*/
+
+//Undefined
+let UserEmail;   //UserEmail=Undefined can also be written like this
+console.log(UserEmail);
+console.log(typeof UserEmail);
+
