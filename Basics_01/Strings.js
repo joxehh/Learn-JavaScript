f8ac3b8073a9e888709037431d990513735449a1
jo <158077622+joxehh@users.noreply.github.com>
2024-09-30 10:11:05 +0000

    strings

---
+++ Basics_01/Strings.js
@@ -0,0 +1,9 @@
+//srings are characters that can be shown in a '' and ""
+let a="hello"+' world'
+console.log(a);
+
+//how to use variable values in log along with some data within
+
+const name="Jo"
+const age=20;
+console.log(`my masters name is ${name} and he is ${age} years young.`);
